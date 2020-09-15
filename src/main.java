0fa6e9fdee82d90326fdd4da10891ba2fd992100
amer293 <amerzahirovic@outlook.com>
2020-09-15 12:18:25 +0200

    if to switch statment

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+
 /**
  /@Title: Rekenmachine
  /@Author: Amer Zahirovic
@@ -6,56 +7,53 @@
  */
 
 import java.util.Scanner;
+
 public class main {
 
-    //Define + - / *
+    // Define + - / *
     final static char PLUS = '+';
     final static char SUBTRACT = '-';
     final static char DIVIDE = '/';
     final static char MULTIPLY = '*';
     final static char EXIT = 's';
 
-    static void PrintCalculation(char operator, double num1, double num2)
-    {
-        //Calculates
-        if(operator == PLUS)
-        {
-            double sum = num1 + num2;
-            System.out.println(num1 + " + " + num2 + " = " +sum);
-        }
+    static void PrintCalculation(char operator, double num1, double num2) {
+        // Calculates
 
-        if(operator == SUBTRACT)
-        {
-            double sum = num1 - num2;
-            System.out.println(num1 + " - " + num2 + " = " +sum);
-        }
+        switch (operator) {
+            case PLUS:
+                double sumPlus = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + sumPlus);
+                break;
 
-        if(operator == DIVIDE)
-        {
-            double sum = num1 / num2;
-            System.out.println(num1 + " / " + num2 + " = " +sum);
-        }
+            case SUBTRACT:
+                double sumSubtract = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + sumSubtract);
+                break;
 
-        if(operator == MULTIPLY)
-        {
-            double sum = num1 * num2;
-            System.out.println(num1 + " * " + num2 + " = " +sum);
+            case DIVIDE:
+                double sumDivide = num1 / num2;
+                System.out.println(num1 + " / " + num2 + " = " + sumDivide);
+                break;
+
+            case MULTIPLY:
+                double sumMultiply = num1 * num2;
+                System.out.println(num1 + " * " + num2 + " = " + sumMultiply);
+                break;
         }
     }
 
-    static boolean IsValidOperator(char character)
-    {
-        //Status code to exit program
+    static boolean IsValidOperator(char character) {
+        // Status code to exit program
         final int STATUS = 0;
 
-        if (character == PLUS || character == SUBTRACT || character == DIVIDE || character == MULTIPLY)
-        {
+        if (character == PLUS || character == SUBTRACT || character == DIVIDE || character == MULTIPLY) {
             return true;
 
-        }else if(character == EXIT){
+        } else if (character == EXIT) {
             System.out.println("Bedankt voor het gebruiken van mijn rekenmachine!");
             System.exit(STATUS);
-        } else{
+        } else {
             System.out.println("Operator is ongeldig!");
             System.out.println();
         }
@@ -63,52 +61,49 @@ public class main {
         return false;
     }
 
-    public static void main(String[] args)
-    {
-        //The defines the first position for getting the char
+    public static void main(String[] args) {
+        // The defines the first position for getting the char
         final int FIRST_POSITION = 0;
 
-        //Repeats everything till user stops the calculator when pressing s
-        while(true)
-        {
+        // Repeats everything till user stops the calculator when pressing s
+        while (true) {
             Scanner input = new Scanner(System.in);
 
-            //Asks for input and puts it to lower case
+            // Asks for input and puts it to lower case
             System.out.print("Kies een operator (S = stoppen): ");
             String c = input.nextLine().toLowerCase();
             System.out.println("De karakter die u heeft ingevuld: " + c);
 
-            //While an empty string is given when choosing an operator it will repeat itself
-            while(c.isEmpty())
-            {
-                System.out.print("Kies een operator (S = stoppen): ");
-                String newC = c = input.nextLine().toLowerCase();
-                System.out.println("De karakter die u heeft ingevuld: " + newC);
+            // While an empty string is given when choosing an operator it will repeat
+            // itself
+            while (c.isEmpty()) {
+                System.out.print("Kies een geldige operator (S = stoppen): ");
+                c = input.nextLine().toLowerCase();
+                System.out.println(c);
             }
 
             System.out.println();
 
-            //Gets the first char
+            // Gets the first char
             char firstChar = c.charAt(FIRST_POSITION);
 
-            //While the operator is not valid repeat
-            while (!IsValidOperator(firstChar))
-            {
+            // While the operator is not valid repeat
+            while (!IsValidOperator(firstChar)) {
                 System.out.print("Kies een geldige operator (S = stoppen): ");
-                String newC  = input.next().toLowerCase();
+                String newC = input.next().toLowerCase();
                 firstChar = newC.charAt(FIRST_POSITION);
                 System.out.println();
                 System.out.println("De karakter die u heeft ingevuld: " + firstChar);
             }
 
-            //Enter num1 and num2
+            // Enter num1 and num2
             System.out.print("Eerste getal: ");
             double num1 = input.nextDouble();
             System.out.print("Tweede getal: ");
             double num2 = input.nextDouble();
 
-            //Calculates sum
-            PrintCalculation(firstChar, num1, num2 );
+            // Calculates sum
+            PrintCalculation(firstChar, num1, num2);
             System.out.println();
         }
 
